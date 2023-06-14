060f31a17cd049e8653c5e2ea4ecb7a10f3b060e
Vescik <wescik992@gmail.com>
2023-06-14 23:28:23 +0200

    added componend with embeded twitch player

---
+++ src/components/StreamModal.vue
@@ -0,0 +1,31 @@
+
+<template>
+    
+    <div class="stream-modal_box">
+        <div id="twitch-player"></div>
+    </div>
+    <div @click="streamModalStore.closeModal" class="backdrop"></div>
+</template>
+
+<script setup lang="ts">
+import { useModalStore } from "@/store/StreamModalStore";
+import * as Twitch from 'twitch-player';
+import{onMounted} from 'vue'
+const streamModalStore = useModalStore();
+
+onMounted(()=> {
+
+    const embed = new Twitch.TwitchEmbed('twitch-player', {
+        width: 854,
+        height: 480,
+        channel: "monstercat",
+        layout: Twitch.TwitchEmbedLayout.VIDEO_WITH_CHAT
+      });
+})
+
+</script>
+
+
+<style scoped lang="scss">
+@import "../scss/components/stream_modal"
+</style>
