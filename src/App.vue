95d4e4c1d09d88bc8248402e98e2cfd2e78d1937
Vescik <wescik992@gmail.com>
2023-06-25 14:20:51 +0200

    handle login routing

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
-    <header class="header_conteiner">
-      <NavbarMain></NavbarMain>
-    </header>
-    <div class="container">
-      <aside class="aside_menu">
+  <header class="header_conteiner">
+    <NavbarMain></NavbarMain>
+  </header>
+  <div class="container">
+    <aside class="aside_menu">
       <NavbarMenu></NavbarMenu>
     </aside>
     <main class="main_conteiner">
-      <RouterView/>
+      <RouterView />
     </main>
     <StreamModal v-if="streamModalStore.showModal"></StreamModal>
-    </div>
-  
-  
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -20,11 +18,47 @@ import NavbarMain from "./components/NavbarMain.vue";
 import NavbarMenu from "./components/NavbarMenu.vue";
 import StreamModal from "./components/StreamModal.vue";
 
-import {useModalStore} from "./store/StreamModalStore"
-import {ref } from "vue";
+import validateToken from "@/composable/getValid";
+import fetchUser from "@/composable/getSingleUser";
 
-
+import { useUserStore } from "@/store/UserStore";
+import { useModalStore } from "./store/StreamModalStore"
+import { useAccesTokenStore } from "./store/AccesTokenStore";
 const streamModalStore = useModalStore()
+
+import { onBeforeMount,onMounted,ref } from "vue";
+import { useRouter } from "vue-router";
+
+const { getUserData } = fetchUser();
+const userStore = useUserStore()
+const accesTokenStore = useAccesTokenStore()
+
+const router = useRouter()
+
+onBeforeMount(async () => {
+  const isTokenValid = await accesTokenStore.isTokenExp()
+  const isTokenSet = accesTokenStore.isTokenSet()
+
+  if(isTokenSet && isTokenValid){
+    const ID = ref(userStore.user.id)
+    console.log(ID.value);
+    
+    const userData = await getUserData(Number(ID.value))
+    userData.map((user:any) => {
+      userStore.setUser(user)
+    })
+    
+    router.push('/home')
+  }
+  else{
+    console.log('token is not valid')
+    router.push('/login')
+  }
+
+
+})
+
+
 </script>
 <style lang="scss">
 @import "./scss/abstract/_variables.scss";
@@ -34,5 +68,4 @@ const streamModalStore = useModalStore()
 @import "./scss/page/global.scss";
 @import "./scss/layout/_navbar_aside.scss";
 @import "./scss/layout/_main_container.scss";
-
 </style>
