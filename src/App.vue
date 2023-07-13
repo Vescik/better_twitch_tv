073aa3aa180ede7d54ff5494c0f3905e08e83fd9
Vescik <wescik992@gmail.com>
2023-07-14 00:38:43 +0200

    changed app structure to better handle popups

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,10 @@
     </aside>
     <main class="main_container">
       <RouterView />
+      <MultiStreamButton/>
     </main>
-    <StreamModal v-if="streamModalStore.showModal"></StreamModal>
+      <StreamModal v-if="streamModalStore.showModal"></StreamModal>
+      <MultiStreamModal v-if="watchTogetherStore.showMultiModal"/>
   </div>
 </template>
 
@@ -17,6 +19,10 @@
 import NavbarMain from "./components/NavbarMain.vue";
 import NavbarMenu from "./components/NavbarMenu.vue";
 import StreamModal from "./components/StreamModal.vue";
+import MultiStreamButton from "./components/MultiStreamButton.vue";
+import MultiStreamModal from "./components/MultiStreamModal.vue";
+import StreamPlayerModal from "./components/StreamPlayerModal.vue";
+import { ref } from "vue";
 
 import validateToken from "@/composable/getValid";
 import fetchUser from "@/composable/getSingleUser";
@@ -24,7 +30,10 @@ import fetchUser from "@/composable/getSingleUser";
 import { useUserStore } from "@/store/UserStore";
 import { useModalStore } from "./store/StreamModalStore"
 import { useAccesTokenStore } from "./store/AccesTokenStore";
+import { useWatchTogetherStore } from "./store/WatchTogetherStore";
+
 const streamModalStore = useModalStore()
+const watchTogetherStore = useWatchTogetherStore()
 
 import { onBeforeMount} from "vue";
 import { useRouter } from "vue-router";
@@ -32,7 +41,6 @@ import { useRouter } from "vue-router";
 const { getUserData } = fetchUser();
 const userStore = useUserStore()
 const accesTokenStore = useAccesTokenStore()
-
 
 
 const router = useRouter()
@@ -68,4 +76,15 @@ onBeforeMount(async () => {
 @import "./scss/page/global.scss";
 @import "./scss/layout/_navbar_aside.scss";
 @import "./scss/layout/_main_container.scss";
+
+body{
+  overflow-x: hidden;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .1s;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 1;
+}
 </style>
